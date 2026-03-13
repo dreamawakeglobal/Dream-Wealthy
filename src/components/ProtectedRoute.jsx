f55ce2289d306e95275e745152bfa549665880ce
Dream Awake Global <dreamawakeglobal@gmail.com>
2026-03-12 22:45:30 -0500

    Allow Tariq.west2496@gmail.com access

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -5,7 +5,10 @@ import { useAuth } from '../contexts/AuthContext';
 export const ProtectedRoute = ({ children }) => {
     const { user } = useAuth();
 
-    const ALLOWED_EMAILS = ['w.shamerer@gmail.com']; // We can add more here if needed later
+    const ALLOWED_EMAILS = [
+        'w.shamerer@gmail.com',
+        'Tariq.west2496@gmail.com'
+    ]; // We can add more here if needed later
 
     // If no user is logged in, OR the user is logged in but is NOT the allowed email
     if (!user || !ALLOWED_EMAILS.includes(user.email)) {
